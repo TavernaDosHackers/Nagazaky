7eb070099e100b1cbfa1d337ca43b5e3ad30a02a
Ygor Simões <ygor@tavernadoshackers.com.br>
2020-09-19 10:00:42 -0300

    Nova versão do Nagazaky

---
+++ New Version/Nagazaky/Nagazaky.cs
@@ -0,0 +1,20 @@
+﻿using Nagazaky.Source;
+using System;
+
+namespace Nagazaky
+{
+    class Nagazaky
+    {
+        static void Main(string[] args)
+        {
+            // Intância de Objetos
+            Strings S = new Strings();
+            
+            Console.WriteLine("Olá " + S.Red + " Olá");
+
+            
+            // Final do Programa
+            Console.ReadLine();
+        }
+    }
+}
---
+++ New Version/Nagazaky/Source/Strings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nagazaky.Source
+{
+    public class Strings
+    {
+        // Atributos (Getters e Setters)
+        public string Red { get; private set; } = " \u001b[31m";
+    }
+}
