ad704b87fb78af09ab4a884003aae32a3d69a457
Ygor Simões <ygor@tavernadoshackers.com.br>
2020-11-01 00:18:18 -0300

    Under development...
    [*] Create:
        - core/config.json
        - extras/user-agents/chrome.txt
        - extras/user-agents/edge.txt
        - extras/user-agents/firefox.txt
        - extras/user-agents/opera.txt
        - extras/user-agents/safari.txt
    
    [*] Modified:
        - nagazaky.py
        - nagazaky/*
        - tests/__init__.py

--- nagazaky.py
+++ nagazaky.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
 """
-ATTENTION! We recommend installing the script before you even run this script.
-Read carefully the steps to install: https://github.com/TavernaDosHackers/Nagazaky/blob/master/README.md
+MIT License
+
+Copyright (c) 2020 Taverna dos Hackers
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
+
+
+# ATTENTION! We recommend installing the script before you even run this script.
+# Read carefully the steps to install: https://github.com/TavernaDosHackers/Nagazaky/blob/master/README.md
 
 from nagazaky.core.color import Color
 from nagazaky import __main__
 
-import sys
-import asyncio
 from platform import python_version
+import sys
 
 if __name__ == "__main__":
     # Verify python version
